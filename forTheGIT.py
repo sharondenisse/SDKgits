63655b64edfeac0f3c4d2a6eed99ca0bbcc9f667
Sharon D. Kenny <sharon.d.kenny@gmail.com>
2018-08-13 12:16:43 -0400

    Deleted file txt

--- forTheGIT.py
+++ forTheGIT.py
@@ -1,6 +1,22 @@
+# Sharon D. Kenny
 # Writing a program for git
+# -----------------------------
+# Edit file by deleting active code
+# and using commented lines instead
 
-x = 3
-y = .14
-pii = x + y
-print(pii)
+print("Hello world")
+#print('Hello world!')
+
+def happy():
+	return ("Hello!")
+
+happy()
+
+# Use this code to change the file
+#def happy():
+#	x = 3
+#	y = .141592
+#	pii = x + y
+#	print(pii)
+#
+#happy()
