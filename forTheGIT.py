60903449324727abab9ce13130aa0f1fffcf3dcb
Sharon D. Kenny <sharon.d.kenny@gmail.com>
2018-09-07 22:11:42 -0400

    Changed the code by uncommenting the 2nd function

--- forTheGIT.py
+++ forTheGIT.py
@@ -5,18 +5,17 @@
 # and using commented lines instead
 
 print("Hello world")
-#print('Hello world!')
 
 def happy():
 	return ("Hello!")
 
 happy()
 
-# Use this code to change the file
-#def happy():
-#	x = 3
-#	y = .141592
-#	pii = x + y
-#	print(pii)
-#
-#happy()
+ Use this code to change the file
+def happy():
+	x = 3
+	y = .141592
+	pii = x + y
+	print(pii)
+
+happy()
